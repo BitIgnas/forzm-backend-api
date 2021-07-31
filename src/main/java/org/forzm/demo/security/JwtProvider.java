2e35854d4212207dcf8a1b83c05118d9f3042ea1
IgnatLT79 <Ignotas99@gmail.com>
2021-07-31 12:34:54 +0300

    jwt

---
+++ src/main/java/org/forzm/demo/security/JwtProvider.java
@@ -0,0 +1,49 @@
+package org.forzm.demo.security;
+
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.*;
+import java.security.cert.CertificateException;
+
+@Service
+@AllArgsConstructor
+public class JwtProvider {
+
+    @Value("${jwt.expiration.time}")
+    private Long jwtExpirationMilli;
+
+    @Value("${jwt.keystore.password}")
+    private String keystorePassword;
+
+    private KeyStore keyStore;
+
+    @PostConstruct
+    public void init() {
+        try {
+            keyStore = KeyStore.getInstance("JKS");
+            InputStream inputStream = getClass().getResourceAsStream("/keystore.jks");
+            keyStore.load(inputStream, keystorePassword.toCharArray());
+        } catch (KeyStoreException | IOException | NoSuchAlgorithmException | CertificateException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public PrivateKey getPrivateKey() {
+        try {
+            return (PrivateKey) keyStore.getKey("keystore", keystorePassword.toCharArray());
+        } catch (KeyStoreException | NoSuchAlgorithmException | UnrecoverableKeyException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+
+
+
+}
